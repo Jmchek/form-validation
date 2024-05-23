feb7fd3eaf7cf8b9a8d6564c487ad6c49375bd3d
Joe Chek <jmchek@gmail.com>
2024-05-23 15:20:12 -0700

    add password confirmation validation

--- index.js
+++ index.js
@@ -20,16 +20,21 @@ window.addEventListener("load", () => {
     email.value.length === 0 || emailRegExp.test(email.value);
   const isCountryValid = country.value == "None";
   const isZipValid = zip.value.length === 0 || zipCodeRegExp.test(zip.value);
-  const isPasswordValid = password.value.length === 0 || pwRegExp.test(password.value)
-  
+  const isPasswordValid =
+    password.value.length === 0 || pwRegExp.test(password.value);
+  const isPasswordConfirmValid =
+    password.value === passwordConfirm.value;
+
   email.className = isEmailValid ? "valid" : "invalid";
   country.className = isCountryValid ? "valid" : "invalid";
   zip.className = isZipValid ? "valid" : "invalid";
   password.className = isPasswordValid ? "valid" : "invalid";
+  passwordConfirm.className = isPasswordConfirmValid ? "valid" : "invalid";
 });
 
 email.addEventListener("input", () => {
-  const isEmailValid = email.value.length === 0 || emailRegExp.test(email.value);
+  const isEmailValid =
+    email.value.length === 0 || emailRegExp.test(email.value);
 
   if (isEmailValid) {
     email.className = "valid";
@@ -37,52 +42,70 @@ email.addEventListener("input", () => {
     emailError.className = "emailError";
   } else {
     email.className = "invalid";
-  } 
+  }
 });
 
 country.addEventListener("change", () => {
-    const isCountryValid = country.value !== "None";
+  const isCountryValid = country.value !== "None";
 
-    if(isCountryValid) {
-        country.className = "valid";
-        countryError.textContent = "";
-        countryError.className = "countryError";
-    } else {
-        country.className = "invalid";
-    }
+  if (isCountryValid) {
+    country.className = "valid";
+    countryError.textContent = "";
+    countryError.className = "countryError";
+  } else {
+    country.className = "invalid";
+  }
 });
 
 zip.addEventListener("input", () => {
-    const isZipValid = zip.value.length === 0 || zipCodeRegExp.test(zip.value);
-  
-    if (isZipValid) {
-      zip.className = "valid";
-      zipError.textContent = "";
-      zipError.className = "zipError";
-    } else {
-      zip.className = "invalid";
-    }  
-  });
+  const isZipValid = zip.value.length === 0 || zipCodeRegExp.test(zip.value);
 
-  password.addEventListener("input", () => {
-    const isPasswordValid = password.value.length === 0 || pwRegExp.test(password.value)
-  
-    if (isPasswordValid) {
-      password.className = "valid";
-      passwordError.textContent = "";
-      passwordError.className = "passwordError";
-    } else {
-        password.className = "invalid";
-    }  
-  });
+  if (isZipValid) {
+    zip.className = "valid";
+    zipError.textContent = "";
+    zipError.className = "zipError";
+  } else {
+    zip.className = "invalid";
+  }
+});
+
+password.addEventListener("input", () => {
+  const isPasswordValid =
+    password.value.length === 0 || pwRegExp.test(password.value);
+
+  if (isPasswordValid) {
+    password.className = "valid";
+    passwordError.textContent = "";
+    passwordError.className = "passwordError";
+  } else {
+    password.className = "invalid";
+  }
+});
+
+passwordConfirm.addEventListener("input", () => {
+  const isPasswordConfirmValid =
+    password.value === passwordConfirm.value;
+
+  if (isPasswordConfirmValid) {
+    password.className = "valid";
+    passwordConfirm.className = "valid"
+    pwConfirmError.textContent = "";
+    pwConfirmError.className = "pwConfirmError";
+  } else {
+    password.className = "valid";
+    passwordConfirm.className = "invalid";
+  }
+});
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   const isEmailValid = emailRegExp.test(email.value);
   const isZipValid = zipCodeRegExp.test(zip.value);
-  const isPasswordValid = password.value.length != 0 || pwRegExp.test(password.value)
-
+  const isPasswordValid =
+    password.value.length != 0 || pwRegExp.test(password.value);
+  const isPasswordConfirmValid =
+    password.value === passwordConfirm.value;
 
   if (!isEmailValid) {
     email.className = "invalid";
@@ -91,10 +114,10 @@ form.addEventListener("submit", (event) => {
   } else {
     email.className = "valid";
     emailError.textContent = "";
-    emailError.className = "zipError";
+    emailError.className = "emailError";
   }
 
-  if(country.value == "None"){
+  if (country.value == "None") {
     country.className = "invalid";
     countryError.textContent = "Please select a country!";
     countryError.className = "countryError active";
@@ -103,19 +126,36 @@ form.addEventListener("submit", (event) => {
   if (!isZipValid) {
     zip.className = "invalid";
     zipError.textContent = "Please provide a valid zip code!";
-    zipError.className = "emailError active";
+    zipError.className = "zipError active";
   } else {
     zip.className = "valid";
     zipError.textContent = "";
     zipError.className = "zipError";
   }
 
-  if(!isPasswordValid){
-    password.className = "valid";
-    passwordError.textContent = "Please provide a valid password!";
-    passwordError.className = "passwordError";
-  } else {
+  if (!isPasswordValid) {
     password.className = "invalid";
+    passwordError.textContent = "Please provide a valid password!";
+    passwordError.className = "passwordError active";
+  } else {
+    password.className = "valid";
+    passwordError.textContent = "";
+    passwordError.className = "passwordError";
+  }
 
+  if (!isPasswordConfirmValid) {
+    passwordConfirm.className = "invalid";
+    password.className = "invalid";
+    pwConfirmError.textContent = "Password confirmation does not match!";
+    passwordError.textContent = "Password confirmation does not match!";
+    pwConfirmError.className = "pwConfirmError active";
+    passwordError.className = "passwordError active";
+  } else {
+    passwordConfirm.className = "valid";
+    password.className = "valid";
+    pwConfirmError.textContent = "";
+    passwordError.textContent = "";
+    pwConfirmError.className = "pwConfirmError";
+    passwordError.className = "passwordError";
   }
 });
